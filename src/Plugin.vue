5db46bb31eb9821c7e6778ada5d14c1c1bd193a1
Dominic Garms <djgarms@gmail.com>
2019-01-29 11:54:19 +0800

    minor change

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -78,20 +78,4 @@
   }
 </script>
 
-<style>
-    .p-metatags__google-title {
-        color: blue;
-        text-decoration: underline;
-    }
 
-    .p-metatags__google-link {
-        color: green;
-    }
-
-    .p-metatags__preview {
-        margin: 5px 0 15px;
-        padding: 10px;
-        color: #000;
-        background: #FFF;
-    }
-</style>
